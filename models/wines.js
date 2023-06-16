ad93f3aa7c73ae30cf078753177275a3d5e1533d
gabbyb21 <gabbailo94@gmail.com>
2023-06-16 14:02:52 -0400

    initial commit

---
+++ models/wines.js
@@ -0,0 +1,17 @@
+const wines = [
+  {name:'Miraval Provence Rose', 
+  type:'pink', 
+  origin: 
+  description:'This pink wine dances with elegance and charm, captivating the senses with its delicate hue and refreshing character. Delightfully pale pink, it exudes a subtle radiance, reminiscent of a sunset over a summer horizon.', 
+  img: },
+  {name:'Bosco', 
+  type:'red', 
+  origin: 
+  description: 'This robust red wine is a masterpiece of boldness and depth. Its deep crimson hue glimmers in the glass, hinting at the concentration of flavors that await.', 
+  img: },
+  {name:'Cava', 
+  type:'white', 
+  origin: 
+  description:'This white wine is a sublime expression of elegance and freshness. Its pale golden hue glistens in the glass, hinting at the radiant flavors that await. As you raise the glass to your nose, a captivating aroma unfolds, filling the air with a symphony of enticing scents.', 
+  img: }
+];
